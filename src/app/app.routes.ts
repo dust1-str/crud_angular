1c126b795cf4a441bebb4964271da7190ac956cd
TheShadow2212 <22170166@uttcampus.edu.mx>
2024-03-03 16:05:06 -0600

    Agregado del elemento Table y empleados para la muestra en un index personalizable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
-    { path: 'home', component: HomeComponent, canActivate: [authGuard]}
+    { path: 'home', component: HomeComponent, canActivate: [authGuard]},
+    { path: 'empleados', component: EmpleadosComponent, canActivate: [authGuard] }
+
 ];
